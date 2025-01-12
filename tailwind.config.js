ae5ac2db33451443b651614bd030916747299643
chang <69925847+changvijay@users.noreply.github.com>
2025-01-13 01:14:28 +0530

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+
+
 module.exports = {
+  safelist: [
+    'hidden',
+    'duration-200',
+    'ease-linear',
+    // Add other classes used dynamically
+  ],
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+    "./public/index.html",
+    ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+  ],
 };
