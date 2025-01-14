febdc1612641b83f07129b7c39caf856ac1d7f3f
chang <69925847+changvijay@users.noreply.github.com>
2025-01-14 17:48:22 +0530

    Update home.js

--- src/template/home.js
+++ src/template/home.js
@@ -330,7 +330,7 @@ export  function Home() {
                   <img src="/3.png" class="absolute block -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
               </div>
               <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                  <img src="/pudhariaman.jpg" class="absolute block -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
+                  <img src="/kuluthiyaman.jpg" class="absolute block -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
               </div>
               <div class="hidden duration-700 ease-in-out" data-carousel-item>
                   <img src="/2.jpeg" class="absolute block -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
